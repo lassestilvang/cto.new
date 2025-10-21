b07af7879bb909c6904eb5527ea474a250cee60b
Lasse Stilvang <lasse.stilvang@gmail.com>
2025-10-21 23:04:15 +0200

    test(jest): configure jest for esm support and coverage
    - Add ts-jest ESM preset and remove manual transform
    - Ignore e2e tests in testPathIgnorePatterns
    - Add transform ignore for nanoid and treat .ts as ESM
    - Collect coverage from lib files and add reporters

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,22 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
-  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/test/setup-jest.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/drizzle/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/drizzle/', '/e2e/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(nanoid)/)',
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+  collectCoverageFrom: [
+    'lib/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+  ],
+  coverageReporters: ['text', 'lcov', 'html'],
 };
 
 export default config;
