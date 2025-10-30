896838254e0734501433c5171eb83e58d1c64b7b
Lasse Stilvang <lasse.stilvang@gmail.com>
2025-10-30 19:48:06 +0100

    chore(config): add PostgreSQL dialect to drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./db/schema.ts",
   out: "./drizzle",
+  dialect: "postgresql" as const,
   driver: "pg",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL || ""
